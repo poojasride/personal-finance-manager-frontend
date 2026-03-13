07b91f682260e80481e53de227255459be51b323
Pooja <poojasri.s@skematix.in>
2026-03-13 19:11:02 +0530

    api url render api updated

--- src/api/exportApi.js
+++ src/api/exportApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api";
+const API_URL = "https://personal-finance-manager-backend-n06b.onrender.com/api";
 
 export const downloadCSV = async (from, to) => {
 
--- src/api/goalApi.js
+++ src/api/goalApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/goals";
+const API_URL = "https://personal-finance-manager-backend-n06b.onrender.com/api/goals";
 
 export const getGoals = async () => {
   const res = await axios.get(API_URL);
